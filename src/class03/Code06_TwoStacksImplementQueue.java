7a123604ef20a28c6830efd425d54c3f3d02fbdc
ext.ouyangcheng1 <ext.ouyangcheng1@jd.com>
2023-05-28 17:25:14 +0800

    栈实现队列

--- src/class03/Code06_TwoStacksImplementQueue.java
+++ src/class03/Code06_TwoStacksImplementQueue.java
@@ -4,57 +4,57 @@ import java.util.Stack;
 
 public class Code06_TwoStacksImplementQueue {
 
-	public static class TwoStacksQueue {
-		public Stack<Integer> stackPush;
-		public Stack<Integer> stackPop;
+    public static class TwoStacksQueue {
+        public Stack<Integer> stackPush;
+        public Stack<Integer> stackPop;
 
-		public TwoStacksQueue() {
-			stackPush = new Stack<Integer>();
-			stackPop = new Stack<Integer>();
-		}
+        public TwoStacksQueue() {
+            this.stackPush = new Stack<>();
+            this.stackPop = new Stack<>();
+        }
 
-		// push栈向pop栈倒入数据
-		private void pushToPop() {
-			if (stackPop.empty()) {
-				while (!stackPush.empty()) {
-					stackPop.push(stackPush.pop());
-				}
-			}
-		}
+        // push栈向pop栈倒入数据
+        private void pushToPop() {
+            if (stackPop.isEmpty()) {
+                while (!stackPush.isEmpty()) {
+                    stackPop.push(stackPush.pop());
+                }
+            }
+        }
 
-		public void add(int pushInt) {
-			stackPush.push(pushInt);
-			pushToPop();
-		}
+        public void add(int value) {
+            stackPush.push(value);
+            pushToPop();
+        }
 
-		public int poll() {
-			if (stackPop.empty() && stackPush.empty()) {
-				throw new RuntimeException("Queue is empty!");
-			}
-			pushToPop();
-			return stackPop.pop();
-		}
+        public int poll() {
+            if (stackPop.isEmpty() && stackPush.isEmpty()) {
+                throw new RuntimeException("Queue is empty");
+            }
+            pushToPop();
+            return stackPop.pop();
+        }
 
-		public int peek() {
-			if (stackPop.empty() && stackPush.empty()) {
-				throw new RuntimeException("Queue is empty!");
-			}
-			pushToPop();
-			return stackPop.peek();
-		}
-	}
+        public int peek() {
+            if (stackPop.isEmpty() && stackPush.isEmpty()) {
+                throw new RuntimeException("Queue is empty");
+            }
+            pushToPop();
+            return stackPop.peek();
+        }
+    }
 
-	public static void main(String[] args) {
-		TwoStacksQueue test = new TwoStacksQueue();
-		test.add(1);
-		test.add(2);
-		test.add(3);
-		System.out.println(test.peek());
-		System.out.println(test.poll());
-		System.out.println(test.peek());
-		System.out.println(test.poll());
-		System.out.println(test.peek());
-		System.out.println(test.poll());
-	}
+    public static void main(String[] args) {
+        TwoStacksQueue test = new TwoStacksQueue();
+        test.add(1);
+        test.add(2);
+        test.add(3);
+        System.out.println(test.peek());
+        System.out.println(test.poll());
+        System.out.println(test.peek());
+        System.out.println(test.poll());
+        System.out.println(test.peek());
+        System.out.println(test.poll());
+    }
 
 }
